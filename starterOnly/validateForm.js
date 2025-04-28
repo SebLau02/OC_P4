0b76b6fc8775c50d978a9dd8e86700fbcd4c11b9
Sébastien Lau <sebastien.lau2021@gmail.com>
2025-04-28 19:39:55 +0200

    add inputs validation

--- starterOnly/validateForm.js
+++ starterOnly/validateForm.js
@@ -1,10 +1,58 @@
 const signupForm = document.getElementById("signup-form");
+const formDataElements = document.querySelectorAll(".formData");
 
+const validationRules = {
+  text: (input) => input.value.length >= 2,
+  email: (input) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value),
+  date: (input) => !isNaN(new Date(input.value)),
+  number: (input) => !isNaN(input.value) && input.value >= 0,
+  checkbox: (input) => input.checked,
+  radio: (input) => input.checked,
+};
+
+function toggleErrorMessage(input, formDataElement) {
+  const isValid = validationRules[input.type](input);
+  formDataElement.dataset.errorVisible = isValid ? "false" : "true";
+}
+
+function validateRadioButtons(formDataElement) {
+  const radios = formDataElement.querySelectorAll("input");
+  //   const isValid = Array.from(radios).some((radio) => radio.checked);
+  //   formDataElement.dataset.errorVisible = isValid ? "false" : "true";
+
+  radios.forEach((radio) => {
+    radio.addEventListener("click", () => {
+      formDataElement.dataset.errorVisible = "false";
+    });
+  });
+}
+
+// Attache les événements d'entrée pour valider en temps réel
+formDataElements.forEach((formDataElement) => {
+  const input = formDataElement.querySelector("input");
+
+  if (input.type === "radio") {
+    validateRadioButtons(formDataElement);
+  } else {
+    input.addEventListener("input", (e) => {
+      toggleErrorMessage(e.target, formDataElement);
+    });
+  }
+});
+
+// Validation lors de la soumission du formulaire
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const formData = new FormData(e.target);
-  console.log(e.target);
-  formData.forEach((value, key) => {
-    console.log(key, value);
+
+  formDataElements.forEach((formDataElement) => {
+    const input = formDataElement.querySelector("input");
+
+    if (input.type === "radio") {
+      const radios = formDataElement.querySelectorAll("input");
+      const isValid = Array.from(radios).some((radio) => radio.checked);
+      formDataElement.dataset.errorVisible = isValid ? "false" : "true";
+    } else {
+      toggleErrorMessage(input, formDataElement);
+    }
   });
 });
