f3fdac970e2e538ae17c779a052b03c8e926a39d
Sébastien Lau <sebastien.lau2021@gmail.com>
2025-04-28 17:43:15 +0200

    add toggleModal function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,11 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalBtns = document.querySelectorAll(".toggle-modal-btn");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtns.forEach((btn) => btn.addEventListener("click", toggleModal));
 
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+function toggleModal() {
+  modalbg.classList.toggle("active");
 }
-
-
